1ae84b1e64100a75e7ff0e06f9a6c999e500f5f5
htrojan <h.trojan.github@posteo.de>
2022-10-09 14:03:04 +0200

    Some stuff is working I think. Great commit message for future Hendriks

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,9 +1,11 @@
+use std::cmp::min;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy::render::mesh::{Indices, PrimitiveTopology};
+use bevy::sprite::Material2d;
 use bevy::window::{close_on_esc, WindowResized};
 
-const COLUMNS: u32 = 7;
+const COLS: u32 = 7;
 const ROWS: u32 = 6;
 
 fn main() {
@@ -13,6 +15,8 @@ fn main() {
         // .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
         .add_system(highlight_system)
+        .add_system(stone_spawn_system)
+        .add_system(stone_moving_system)
         .add_system(resize_handler)
         .add_system(close_on_esc)
         .run();
@@ -26,15 +30,48 @@ struct HighlightColumn {
     number: u32,
 }
 
-struct HighlightMeta {
+enum Player {
+    Player1,
+    Player2
+}
+
+struct BoardAssets {
     highlight_size: Vec2,
     left_coord: f32,
+    mesh_handle: Handle<Mesh>,
+    // board: [[Player; COLS as usize]; ROWS as usize]
+}
+
+impl BoardAssets {
+    fn stone_position(&self, game_position: UVec2) -> Vec2 {
+        let down_coord = -self.highlight_size.y / 2.;
+
+        let x_pos = self.left_coord + game_position.x as f32 * self.highlight_size.x;
+        let y_pos = down_coord + game_position.y as f32 * (self.highlight_size.y / ROWS as f32);
+        Vec2::new(x_pos, y_pos)
+    }
 }
 
 struct BoardShape {
     size: Vec2,
     rows: u32,
     cols: u32,
+}
+
+#[derive(Component)]
+struct Stone {
+    /// If stone is still moving onto its goal
+    moving: bool,
+    velocity: f32,
+    /// The final board position the stone will land on in the game
+    board_position: UVec2,
+}
+
+/// Stores information how the player stones should be rendered
+struct StoneAssets {
+    /// Materials for player one and two
+    mat_handle: [Handle<ColorMaterial>; 2],
+    mesh_handle: Handle<Mesh>,
 }
 
 impl From<BoardShape> for Mesh {
@@ -86,15 +123,32 @@ fn setup(mut commands: Commands, mut windows: ResMut<Windows>, mut meshes: ResMu
     let width = window.width();
     let height = window.height();
 
-    let highlight_size = Vec2::new(width / COLUMNS as f32, height);
+    let highlight_size = Vec2::new(width / COLS as f32, height);
     let left_coord = -width / 2. + highlight_size.x / 2.;
+
+    let diameter = ((width / COLS as f32).min(height / ROWS as f32));
+    let stone_mesh = meshes.add(Mesh::from(shape::Circle::new(diameter / 2.)));
+    let stone_mat_1 = materials.add(ColorMaterial {
+        color: Color::rgba(0.8, 0.1, 0., 1.),
+        texture: None,
+    });
+    let stone_mat_2 = materials.add(ColorMaterial {
+        color: Color::rgba(1., 1., 0., 1.),
+        texture: None,
+    });
+
+    let stone_meta = StoneAssets {
+        mat_handle: [stone_mat_1, stone_mat_2],
+        mesh_handle: stone_mesh,
+    };
+    commands.insert_resource(stone_meta);
 
     println!("Creating board mesh");
     let board_mesh = meshes.add(
         Mesh::from(BoardShape {
             size: Vec2::new(1., 1.),
             rows: ROWS,
-            cols: COLUMNS,
+            cols: COLS,
         })
     );
     let board_material = materials.add(
@@ -103,7 +157,7 @@ fn setup(mut commands: Commands, mut windows: ResMut<Windows>, mut meshes: ResMu
 
     commands.spawn_bundle(
         ColorMesh2dBundle {
-            mesh: board_mesh.into(),
+            mesh: board_mesh.clone().into(),
             material: board_material,
             transform: Transform::default().with_scale(Vec3::new(width, height, 1.)),
             ..default()
@@ -113,9 +167,10 @@ fn setup(mut commands: Commands, mut windows: ResMut<Windows>, mut meshes: ResMu
     ).insert(Board);
 
     commands.insert_resource(
-        HighlightMeta {
+        BoardAssets {
             highlight_size,
             left_coord,
+            mesh_handle: board_mesh,
         }
     );
 
@@ -149,16 +204,16 @@ fn setup(mut commands: Commands, mut windows: ResMut<Windows>, mut meshes: ResMu
 }
 
 fn resize_handler(mut ev_reader: EventReader<WindowResized>, mut query: Query<&mut Transform, With<Board>>,
-                  mut meta: ResMut<HighlightMeta>) {
+                  mut meta: ResMut<BoardAssets>) {
     for ev in ev_reader.iter() {
         let mut t = query.single_mut();
         t.scale = Vec3::new(ev.width, ev.height, 1.);
-        meta.highlight_size = Vec2::new(ev.width / COLUMNS as f32, ev.height);
-        meta.left_coord = - ev.width / 2. + ev.width/(2*COLUMNS) as f32 ;
+        meta.highlight_size = Vec2::new(ev.width / COLS as f32, ev.height);
+        meta.left_coord = -ev.width / 2. + ev.width / (2 * COLS) as f32;
     }
 }
 
-fn highlight_system(windows: Res<Windows>, mut query: Query<(&mut HighlightColumn, &mut Transform)>, highlight_meta: Res<HighlightMeta>) {
+fn highlight_system(windows: Res<Windows>, mut query: Query<(&mut HighlightColumn, &mut Transform)>, highlight_meta: Res<BoardAssets>) {
     let window = windows.get_primary().unwrap();
     if let Some(position) = window.cursor_position() {
         let new_column: u32 = (position.x / highlight_meta.highlight_size.x) as u32;
@@ -172,13 +227,44 @@ fn highlight_system(windows: Res<Windows>, mut query: Query<(&mut HighlightColum
         t.scale = Vec3 {
             x: highlight_meta.highlight_size.x,
             y: highlight_meta.highlight_size.y,
-            z: 0.0
+            z: 0.0,
         };
         col.number = new_column;
-
-        // cursor is inside of window
-        // println!("x = {}, y = {}", position.x, position.y);
     } else {
         // Cursor is outside of window
+    }
+}
+
+fn stone_spawn_system(mut commands: Commands, stone_meta: Res<StoneAssets>, mouse: Res<Input<MouseButton>>,
+                      mut query: Query<&HighlightColumn>, meta: Res<BoardAssets>, windows: Res<Windows>) {
+    if mouse.just_pressed(MouseButton::Left) {
+        let height = windows.primary().height();
+        let col = query.single().number;
+        let xpos = col as f32 * meta.highlight_size.x + meta.left_coord;
+
+        commands.spawn_bundle(ColorMesh2dBundle {
+            mesh: stone_meta.mesh_handle.clone().into(),
+            material: stone_meta.mat_handle[0].clone().into(),
+            transform: Transform::from_xyz(xpos, height / 2., 10.),
+            ..default()
+        }).insert(Stone {
+            moving: true,
+            velocity: -300.,
+            board_position: UVec2::new(col, 0),
+        });
+    }
+}
+
+fn stone_moving_system(mut query: Query<(&mut Transform, &mut Stone)>, time: Res<Time>, board: Res<BoardAssets>) {
+    for (mut t, mut stone) in query.iter_mut() {
+        // t.translation += Vec3::new();
+        if stone.moving {
+            t.translation += Vec3::new(0., stone.velocity * time.delta_seconds(), 0.);
+            let goal_pos = board.stone_position(stone.board_position);
+            // info!("goal_pos = {:?}", goal_pos);
+            if t.translation.y <= goal_pos.y {
+                stone.moving = false;
+            }
+        }
     }
 }
